57d60125b1f0bf5a8a238c6ed2cf14359f339244
Ilias-Karimalis <iliaskarimalis@outlook.com>
2020-09-03 11:20:58 -0700

    Refactoring + largeexample

--- bootstrap_percolation.py
+++ bootstrap_percolation.py
@@ -2,16 +2,18 @@ from decimal import *
 from random import randint
 import matplotlib.pyplot as plt
 
-H = height = 25
-W = width  = 25
-size = height * width
+# Default height and width
+H = height = 10
+W = width  = 10
 
+# Instantiates the empty board with initial infection points
 def initialize_board(empty_board, p):
     for i in range(int(p*size)):
         # Currently have multiple initial inits could be placed on same spot
         # Class bday problem could be relevant here!!!
         empty_board[randint(0,height-1)][randint(0,width-1)] = 1
 
+# Executes the percolation algorithm
 def generate_board(initial_board, g):
     for i in range(g):
         new_board = [[0 for x in range(width)] for y in range(height)]
@@ -26,19 +28,20 @@ def generate_board(initial_board, g):
                     new_board[j][k] = i + 1
                 else:
                     new_board[j][k] = initial_board[j][k]
-        print_board(new_board)
+        # print_board(new_board)
         initial_board = new_board
     return new_board
 
+# Prints the board state to the terminal
 def print_board(board):
     print(*board, sep='\n')
 
+# Plots the board state
 def draw_board(board):
     plt.imshow(board)
     plt.show()
 
 if __name__ == '__main__':
-    board = [[0 for x in range(width)] for y in range(height)]
 
     # Initialize the input for the p value"
     while True:
@@ -51,6 +54,28 @@ if __name__ == '__main__':
             print("Please enter a decimal between 0 and 1")
         else:
             break
+
+    # Initialize board dimensions
+    while True:
+        try:
+            H = height = int(input('Set board height: '))
+        except ValueError:
+            print("Please enter an integer")
+        else:
+            break
+
+    while True:
+        try:
+            W = width = int(input('Set board width: '))
+        except ValueError:
+            print("Please enter an integer")
+        else:
+            break
+
+    size = height * width
+
+    # Instantiate empty board
+    board = [[0 for x in range(width)] for y in range(height)]
 
     # Initializes random initial infections
     initialize_board(board, p)
